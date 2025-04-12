fc6a06e817f8328c30506476b3443379fe6c0288
jmmon <jmmon93@gmail.com>
2025-04-12 13:54:27 -0400

    reduce time for deck deal on large decks

--- src/v3/constants/game.ts
+++ src/v3/constants/game.ts
@@ -58,8 +58,26 @@ const DECK_SIZE_DEFAULT = isProd ? 18 : (18 as const);
 const DECK_SIZE_MIN = 6 as const;
 const DECK_SIZE_MAX = 52 as const;
 
+// want to keep it reasonable across all deck sizes
+// so now it takes a base of ~1.5s divided by the cards,
+// and adds a small additional flat value
+// so 52 cards will take longer, but it still starts at the same base value
+// e.g. 1500 base across the cards, + 35ms per card
+//  - e.g. 6 cards == 1500 + 210 => 1710ms / 6 = 285ms per card
+//  - e.g. 18 cards == 1500 + 35*18 = 1500 + 1330 => 1830ms / 18 = ~102ms per card
+//  - e.g. 32 cards == 1500 + 35*32 = 1500 + 70 + 1050 => 2620ms / 32 = ~82ms per card
+//  - e.g. 52 cards == 1500 + 35*52 = 1500 + 1400 + 420 => 3320ms == ~64ms per card
+//
+//  adjust to 25ms percard:
+//  6 => 1500 + 150 => 1650ms total (reduced by 60ms) / 6 = 275ms
+//  52 => 1500 + 25*52 => 1500 + 1300 => 2800ms total (reduced by 520ms) / 52 = 54ms per card
+//
+//adjust to 20ms per card:
+//  6 => 1500 + 120 => 1620 total (total reduction of 90ms) => 270ms each
+//  52 => 1500 + 20*52 => 1500 + 1040 => 2540ms total (total reduction of 780ms) = 49ms per card
+
 const FAN_OUT_DURATION_BASE_MS = 1500 as const;
-const FAN_OUT_DURATION_ADDITIONAL_PER_CARD_MS = 35 as const;
+const FAN_OUT_DURATION_ADDITIONAL_PER_CARD_MS = 20 as const;
 
 /**
  * dictate starting position of deck when dealing out cards on initialization
@@ -67,7 +85,7 @@ const FAN_OUT_DURATION_ADDITIONAL_PER_CARD_MS = 35 as const;
  *    e.g. x:0, y:0.5 for left-center
  *    e.g. perhaps x: -.01, y: 0.5 to start slightly further off the left side
  * percents of board dimensions, so does not take into account the header (padding)
- *
+ 
  * Percents are now adjusted if above 1 or below 0 so they are not real percents.
  *  - They are accurate to the x-y col/row coords if within 0 to 1
  *    e.g. 1 === last column/row, 0 === first column/row
