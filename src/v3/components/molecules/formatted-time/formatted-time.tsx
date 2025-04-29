f7f1500c9ff884a59dad68c666a710f15fca441d
jmmon <jmmon93@gmail.com>
2025-04-28 20:31:50 -0400

    fix consistent ms rounding

--- src/v3/components/molecules/formatted-time/formatted-time.tsx
+++ src/v3/components/molecules/formatted-time/formatted-time.tsx
@@ -1,4 +1,4 @@
-import { formatTimeFromMs, truncateMs } from "~/v3/utils/formatTime";
+import { formatTimeFromMs, msToDs } from "~/v3/utils/formatTime";
 import HeaderSpanGreyedAtZero from "~/v3/components/atoms/header-span-greyed-at-zero/header-span-greyed-at-zero";
 import type { FunctionComponent } from "@builder.io/qwik/jsx-runtime";
 import type { ClassList } from "@builder.io/qwik";
@@ -10,11 +10,10 @@ type FormattedTimeProps = {
 };
 const FormattedTime: FunctionComponent<FormattedTimeProps> = ({
   timeMs,
-  limit = 2,
   classes = "",
 }) => {
   const { minutes, seconds, ms } = formatTimeFromMs(timeMs);
-  const limitedMs = truncateMs(Number(ms), limit);
+  const limitedMs = msToDs(Number(ms));
 
   return (
     <div class={`text-center text-slate-100 ${classes}`}>
